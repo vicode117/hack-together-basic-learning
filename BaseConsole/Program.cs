7dfa598d2eef855d7f376b7015797aeb24216e67
Victor Zhou <vicode117@gmail.com>
2023-03-09 00:32:36 +0800

    feat(base-console): add base console project

---
+++ BaseConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var scopes = new[] { "User.Read" };
+var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+{
+    ClientId = "",
+};
+Console.WriteLine($"{interactiveBrowserCredentialOptions.RedirectUri}");
+var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+Console.WriteLine($"after{interactiveBrowserCredentialOptions.RedirectUri}");
+
+
+var graphClient = new GraphServiceClient(tokenCredential, scopes);
+
+var me = await graphClient.Me.GetAsync();
+var jsonStr = JsonSerializer.Serialize(me);
+
+Console.WriteLine($"Hello {me?.DisplayName}!");
+Console.WriteLine($"user object: {jsonStr}!");
