5c1219ca270dd0ea5a9f0d48a3b03d444e9468d9
Victor Zhou <vicode117@gmail.com>
2023-03-16 01:11:18 +0800

    feat: add GraphHelper; add Settings

--- BaseConsole/Program.cs
+++ BaseConsole/Program.cs
@@ -1,25 +1,106 @@
-﻿using System.Text.Json;
-using Azure.Identity;
-using Microsoft.Graph;
-using Microsoft.Graph.Models;
+﻿Console.WriteLine(".NET Graph Tutorial\n");
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var settings = Settings.LoadSettings();
 
-var scopes = new[] { "User.Read" };
-var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+// Initialize Graph
+InitializeGraph(settings);
+
+// Greet the user by name
+await GreetUserAsync();
+
+int choice = -1;
+
+while (choice != 0)
 {
-    ClientId = "",
-};
-Console.WriteLine($"{interactiveBrowserCredentialOptions.RedirectUri}");
-var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
-Console.WriteLine($"after{interactiveBrowserCredentialOptions.RedirectUri}");
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Display access token");
+    Console.WriteLine("2. List my inbox");
+    Console.WriteLine("3. Send mail");
+    Console.WriteLine("4. Make a Graph call");
+
+    try
+    {
+        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+    }
+    catch (System.FormatException)
+    {
+        // Set to invalid value
+        choice = -1;
+    }
+
+    switch (choice)
+    {
+        case 0:
+            // Exit the program
+            Console.WriteLine("Goodbye...");
+            break;
+        case 1:
+            // Display access token
+            await DisplayAccessTokenAsync();
+            break;
+        case 2:
+            // List emails from user's inbox
+            await ListInboxAsync();
+            break;
+        case 3:
+            // Send an email message
+            await SendMailAsync();
+            break;
+        case 4:
+            // Run any Graph code
+            await MakeGraphCallAsync();
+            break;
+        default:
+            Console.WriteLine("Invalid choice! Please try again.");
+            break;
+    }
+}
 
 
-var graphClient = new GraphServiceClient(tokenCredential, scopes);
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForUserAuth(settings,
+        (info, cancel) =>
+        {
+            // Display the device code message to
+            // the user. This tells them
+            // where to go to sign in and provides the
+            // code to use.
+            Console.WriteLine(info.Message);
+            return Task.FromResult(0);
+        });
+}
 
-var me = await graphClient.Me.GetAsync();
-var jsonStr = JsonSerializer.Serialize(me);
+async Task GreetUserAsync()
+{
+    // TODO
+}
 
-Console.WriteLine($"Hello {me?.DisplayName}!");
-Console.WriteLine($"user object: {jsonStr}!");
+async Task DisplayAccessTokenAsync()
+{
+    try
+    {
+        var userToken = await GraphHelper.GetUserTokenAsync();
+        Console.WriteLine($"User token: {userToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting user access token: {ex.Message}");
+    }
+}
+
+async Task ListInboxAsync()
+{
+    // TODO
+}
+
+async Task SendMailAsync()
+{
+    // TODO
+}
+
+async Task MakeGraphCallAsync()
+{
+    // TODO
+}
